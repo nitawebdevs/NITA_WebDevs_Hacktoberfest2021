613ee08a9ae333826a86301fee8dde1aff3377f8
Ankit <a53gau@gmail.com>
2021-10-11 02:53:34 +0530

    UI changed

---
+++ pass_generator/ankitGautam_nita/app.js
@@ -0,0 +1,6 @@
+function exe() {
+    var name = document.forms["reg"]["name"].value;
+    var number = document.forms["reg"]["number"].value;
+    var male = document.getElementById("male").checked = true;
+    var female = document.getElementById("female").checked = true;
+};
